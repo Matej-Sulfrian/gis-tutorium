0115202a249d4c3dd0416af08a6479c3e41b950f
Matej-Sulfrian <matej.sulfrian@hs-furtwangen.de>
2023-11-15 22:30:05 +0100

    add try not to laugh

---
+++ proj4tryNotToLaugh/script.js
@@ -0,0 +1,70 @@
+window.addEventListener("load", init);
+
+let jokes;
+let nextBtn;
+let jokeCountEl;
+let jokeEl;
+let refreshLifeEl;
+
+function init () {
+
+	getJokes();
+
+	nextBtn = document.querySelector(".next")
+	nextBtn.addEventListener("click", onNextBtnClick)
+
+	jokeCountEl = document.querySelector("#joke-count")
+	jokeEl = document.querySelector("#joke")
+
+	refreshLifeEl = document.querySelector("#refreshLife")
+	refreshLifeEl.addEventListener("click", refreshLife)
+
+}
+
+function onNextBtnClick () {
+
+	if (jokes.length > 0) {
+		console.log("Next joke...")
+
+		jokeEl.innerHTML = jokes[0].joke
+		jokes.shift()
+
+		jokeCountEl.innerHTML = jokes.length + 1
+	} else {
+
+		getJokes()
+	}
+
+
+}
+
+function getJokes() {
+
+	 fetch('https://v2.jokeapi.dev/joke/Any?type=single&amount=10')
+		.then(response => {
+			if (!response.ok) {
+				throw new Error('Network response was not ok');
+			}
+			return response.json();
+		})
+		.then(data => {
+			// handle the JSON data
+
+			jokes = data.jokes
+			jokeCountEl.innerHTML = data.amount
+
+			jokeEl.innerHTML = jokes[0].joke
+			jokes.shift()
+
+		})
+		.catch(error => {
+			console.error('There was a problem with the fetch operation:', error);
+		});
+}
+
+function refreshLife () {
+	let lifeEls = document.querySelectorAll(".icofont-heart")
+	for (let el of lifeEls) {
+		el.classList.add('active')
+	}
+}
